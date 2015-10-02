099c34c2a98fbfb356c9c0a6f42bf83d1596aa1c
Cristian Sarmiento <cccsarmiento@gmail.com>
2015-10-02 03:47:38 -0300

    Render the template of current month

--- js/jquery.jacalendar.js
+++ js/jquery.jacalendar.js
@@ -1,48 +1,78 @@
 ;(function($, window, undefined) {
     'use strict';
 
-    $.jaCal = function(options, element) {    
+    $.jaCal = function(options, element) {
         this.$el = $(element);
         this._init(options);
     };
 
     $.jaCal.defaults = {
-        name:      'jaCal',
-        language:  'en',
-        tmpl_path: 'tmpls/',
-        rows:      5,
-        startIn:   0,
-        template : ''
+        name: 'jaCal',
+        language: 'en',
+        tmpl_path: 'tmpls/',        
+        template: ''
     };
 
     $.jaCal.prototype = {
         _init: function(options) {
             this.settings = $.extend(true, {}, $.jaCal.defaults, options);
-            this.today = moment();                      
+            this.today = moment();
+            this.today.locale(this.settings.language);
             this.render();
-          
+
         },
-        _loadTemplate: function() { 
+        _loadTemplate: function() {
             var self = this;
             if (self.settings.template) {
                 return;
             }
             $.ajax({
-                url : self.settings.tmpl_path + 'month.html',
-                type : 'get',
+                url: self.settings.tmpl_path + 'month.html',
+                type: 'get',
                 async: false,
-                success : function(html) {
-                    self.settings.template = html;
+                success: function(html) {
+                    self.settings.template = _.template(html);
                 }
-    
+
             });
+        },
+        //https://github.com/moment/moment/issues/2433
+        _forLocale: function(locale, fn) {
+            var temp = moment.locale();
+            moment.locale(locale);
+            var result = fn();
+            moment.locale(temp);
+            return result;
+        },
+        _getData: function(){
+            var start = this.today.startOf('month').subtract(this.today.startOf('month').day(), 'day');
+            var array_dates = [];
+            for (var i = 0; i < 6; i++) {
+                var div = $('<div>').addClass('row week-' + (i+1) );
+                for (var j = 0; j < 7; j++) {
+                    var inner_div = $('<div>').addClass('col-1').attr('data-date', start.format('YYYY-MM-DD')).html(parseInt(start.format('DD')));
+                    div.append(inner_div);
+                    start.add(1, 'day')
+                }
+                array_dates.push(div.wrap('<p/>').parent().html());
+            }
+            var month = this.today.format('MMMM');
+            var year = this.today.format('GGGG');
+            return {
+                top: {
+                    month, year
+                },
+                weekdays: this._forLocale(this.settings.language, moment.weekdays),
+                dates: array_dates
+            };
         },
         //public methods
         render: function() {
             this.$el.html('');
-            this._loadTemplate();             
-            this.$el.html(this.settings.template);
+            this._loadTemplate();                        
+            this.$el.append(this.settings.template(this._getData()));
         }
+
     }
 
     $.fn.jaCal = function(options) {
